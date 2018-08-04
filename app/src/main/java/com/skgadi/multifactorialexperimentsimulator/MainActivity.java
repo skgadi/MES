0e0322ab8e36355506d4f635dcee052082cd3de8
Suresh Kumar Gadi <skgadi@users.noreply.github.com>
2018-08-03 21:26:41 -0500

    ver code 1

--- app/src/main/java/com/skgadi/multifactorialexperimentsimulator/MainActivity.java
+++ app/src/main/java/com/skgadi/multifactorialexperimentsimulator/MainActivity.java
@@ -1,13 +1,74 @@
 package com.skgadi.multifactorialexperimentsimulator;
 
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.webkit.JsResult;
+import android.webkit.WebChromeClient;
+import android.webkit.WebView;
 
 public class MainActivity extends AppCompatActivity {
+
+    WebView MainPage;
+    final Context myApp=this;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //--- Var Init
+        setTitle(getString(R.string.app_name));
+        MainPage = findViewById(R.id.MainPage);
+        MainPage.setWebChromeClient(new WebChromeClient(){
+            @Override
+            public boolean onJsAlert(WebView view, String url, String message, final android.webkit.JsResult result)
+            {
+                new AlertDialog.Builder(myApp)
+                        .setTitle("Simmon says...")
+                        .setMessage(message)
+                        .setPositiveButton(android.R.string.ok,
+                                new AlertDialog.OnClickListener()
+                                {
+                                    public void onClick(DialogInterface dialog, int wicht)
+                                    {
+                                        result.confirm();
+                                    }
+                                }).setCancelable(false)
+                        .create()
+                        .show();
+                return true;
+            }
+            @Override
+            public boolean onJsConfirm(WebView view, String url, String message, final JsResult result) {
+                new AlertDialog.Builder(myApp)
+                        .setTitle("Alert!")
+                        .setMessage(message)
+                        .setPositiveButton(android.R.string.ok,
+                                new DialogInterface.OnClickListener()
+                                {
+                                    public void onClick(DialogInterface dialog, int which)
+                                    {
+                                        result.confirm();
+                                    }
+                                })
+                        .setNegativeButton(android.R.string.cancel,
+                                new DialogInterface.OnClickListener()
+                                {
+                                    public void onClick(DialogInterface dialog, int which)
+                                    {
+                                        result.cancel();
+                                    }
+                                })
+                        .create()
+                        .show();
+
+                return true;
+            }
+        });
+        MainPage.getSettings().setJavaScriptEnabled(true);
+        MainPage.loadUrl("file:///android_asset/main.html");
     }
 }
